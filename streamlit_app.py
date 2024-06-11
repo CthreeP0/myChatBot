f7c390ca892340e8591f5dff32ff0893bf02dc70
CthreeP0 <esfahan.elman@petronas.com>
2024-06-11 16:57:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,10 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 # Function for generating LLM response
-def generate_response(prompt_input):
+def generate_response(prompt_input,email,passwd):
 
     email = "esfahan.elman@petronas.com"
-    passwrd = "Esfahan_181103"
+    passwd = "Esfahan_181103"
     # Hugging Face Login
     sign = Login(email, passwd)
     cookies = sign.login()
